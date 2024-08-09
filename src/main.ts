21a0f741d7a2963d13facea2bba064851d55ffeb
Marco Antonio Juarez <mjuarez@adere.so>
2024-08-09 15:30:19 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from './App.vue'
+import router from './router'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+            light: {
+                colors: {
+                    primary: '#1976D2',
+                    secondary: '#424242',
+                    accent: '#82B1FF',
+                    error: '#FF5252',
+                    info: '#2196F3',
+                    success: '#4CAF50',
+                    warning: '#FB8C00',
+                },
+            },
+            dark: {
+                colors: {
+                    primary: colors.deepPurple.darken1,
+                    secondary: '#64B5F6',
+                    accent: '#03DAC6',
+                    error: '#CF6679',
+                    info: '#2196F3',
+                    success: '#4CAF50',
+                    warning: '#FB8C00',
+                },
+            },
+        },
+    },
+});
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
